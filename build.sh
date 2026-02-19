37b79439f97f5b89ab6bfa8148b5b67dce268346
Eric Spishak-Thomas <emspishak@gmail.com>
2026-02-19 14:13:19 +0000

    Use a specific Node vesion via NVM.

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+export NVM_DIR="$HOME/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+nvm use
+
 REPO="$HOME/files/esstraba"
 
 cd "$REPO/frontend"
